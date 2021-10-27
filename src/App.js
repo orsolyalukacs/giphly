03434bb4d3591af40822a04c506be6472820efbb
orsolyalukacs <orchie.lucas@gmail.com>
2021-10-27 08:12:18 -0500

    refactor states and render. add smaller image format and webp image

--- src/App.js
+++ src/App.js
@@ -1,47 +1,57 @@
 // App.js
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useCallback } from "react";
 import { GIPHY_API_KEY } from '../config';
 
 const App = () => {
   const [gifResults, setGifResults] = useState(null);
   const [searchTerm, setSearchTerm] = useState('');
-  const [trendingGifs, setTrendingGifs] = useState(null);
   const [openedImg, setOpenedImg] = useState('');
   const apiKey = GIPHY_API_KEY;
 
+  // fetch trending gif data from API
   const trendingResults = () => {
     fetch(`http://api.giphy.com/v1/gifs/trending?&api_key=${apiKey}&limit=25`)
       .then(response => response.json())
-      .then(response => setTrendingGifs(response.data))
+      .then(response => {
+        console.log(response.data)
+        setGifResults(response.data)
+      }
+      )
       .catch(err => console.error(err))
   }
 
+  // render trending results on first page load
   useEffect(() => {
     trendingResults();
   }, [])
 
+  // fetch search results from API
   const getGifs = (event) => {
     event.preventDefault();
     fetch(`http://api.giphy.com/v1/gifs/search?q=${searchTerm}&api_key=${apiKey}&limit=25`)
       .then(response => response.json())
       .then(response => {
         setGifResults(response.data)
-        setTrendingGifs(null)
       }).catch(err => console.error(err))
   }
 
+  // set search value from form
   const inputGifName = (event) => {
     setSearchTerm(event.target.value)
   }
 
   const openModal = (gif) => {
-    setOpenedImg(gif);
+    setOpenedImg(gif)
+  }
+
+  const closeModal = () => {
+    setOpenedImg(!openedImg)
   }
 
   return (
     <>
       {openedImg ?
-        <div className="modal" onClick={() => { setOpenedImg(!openedImg) }}>
+        <div className="modal" onClick={closeModal}>
           <img className="image-large" src={openedImg.images.original.url} />
         </div> : null
       }
@@ -56,35 +66,20 @@ const App = () => {
             </input>
           </form>
           {
-            trendingGifs &&
-            <div>
-              <h2 className="sub-title">Trending</h2>
-              <div className="image-grid dw">
-                {
-                  trendingGifs.map(gif =>
-                    <div className="image-container">
-                      <img
-                        key={gif.id}
-                        src={gif.images.downsized_medium.url}
-                        className="gif-image"
-                        onClick={() => openModal(gif)} />
-                    </div>
-                  )
-                }
-              </div>
-            </div>
-          }
-          {
             gifResults &&
             <div className="image-grid dw">
               {
                 gifResults.map(gif =>
                   <div className="image-container">
-                    <img
-                      key={gif.id}
-                      src={gif.images.downsized_medium.url}
-                      className="gif-image"
-                      onClick={() => openModal(gif)} />
+                    <picture>
+                      <source type="img/webp" srcSet="{gif.images.fixed_height_downsampled.webp}" />
+                      <img
+                        key={gif.id}
+                        src={gif.images.fixed_height_downsampled.url}
+                        alt={gif.title}
+                        className="gif-image"
+                        onClick={() => openModal(gif)} />
+                    </picture>
                   </div>
                 )
               }
